a8da9d4c6d63ac49df8052348cbdc5200d73850f
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-05-09 19:36:42 +0100

    Begin work on android port

--- run-android.bat
+++ run-android.bat
@@ -1,7 +1,7 @@
 rd tmp /s /q
 mkdir tmp\PencilJar
 xcopy /E /I %cd%\src\universal %cd%\tmp\PencilJar
-xcopy /E /I %cd%\src\windows %cd%\tmp\PencilJar
+xcopy /E /I %cd%\src\android %cd%\tmp\PencilJar
 cd tmp\PencilJar
 ren main.pde PencilJar.pde
 processing-java --sketch="%cd%" --run
---
+++ src/android/cursor.pde
@@ -0,0 +1,2 @@
+void cursorInit() {}
+void cursorCheckHand() {}
