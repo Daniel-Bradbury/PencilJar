fcc49aabb8d4359c7741a742d1dfdfb920afcfb4
Daniel-Bradbury <danieljamesbradbury@hotmail.com>
2020-05-11 19:39:41 +0100

    Changed the way that platform-specific code is added

--- run-linux.sh
+++ run-linux.sh
@@ -2,7 +2,7 @@ rm -rf tmp
 mkdir tmp
 mkdir tmp/PencilJar
 cp -r src/universal/* tmp/PencilJar
-cp -r src/windows/* tmp/PencilJar
+cp -r src/linux/* tmp/PencilJar
 cd tmp/PencilJar
 mv main.pde PencilJar.pde
 processing-java --sketch="$PWD" --run
