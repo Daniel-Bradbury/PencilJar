22f6bd7e5dba53c5816d478f24a7edac1c2eca25
Daniel-Bradbury <danieljamesbradbury@hotmail.com>
2020-05-19 17:15:33 +0100

    Added patch script

---
+++ patch.py
@@ -0,0 +1,13 @@
+import re
+import sys
+def patch(patchline):
+  with open(re.findall('IN (.*) REPLACE', patchline)[0], 'r+') as codefile:
+    codefilecontent=codefile.read()
+    old = re.findall('REPLACE (.*) WITH', patchline)[0]
+    new = re.findall('WITH (.*);', patchline)[0]
+  with open(re.findall('IN (.*) REPLACE', patchline)[0], 'w+') as codefile:
+    codefile.write(codefilecontent.replace(old, new))
+patchfile=open("PATCH", "r")
+patchfilecontent=patchfile.readlines()
+for x in range(0,len(patchfilecontent)):
+  patch(patchfilecontent[x-1])
